f58a64f6a6ba3936cedc8c37bc2eb3415963637b
Ivan <ivanmercado9@gmail.com>
2019-12-04 08:59:23 -0400

    Adds Attending View

--- resources/js/admin/frontdesk/Attending.vue
+++ resources/js/admin/frontdesk/Attending.vue
@@ -8,10 +8,21 @@
               <v-avatar color="blue darken-1" size="80">
                 <span class="white--text headline">CJ</span>
               </v-avatar>
-              <h3 class="subtitle-2 mt-5">Carlos Joniel Santiago</h3>
-              <h3 class="subtitle-2 my-0 font-weight-light overline gray--text">#302678</h3>
+              <!-- <h3 class="subtitle-2 mt-5">Carlos Joniel Santiago</h3> -->
+              <!-- <h3 class="subtitle-2 my-0 font-weight-light overline gray--text">#302678</h3> -->
               <!-- <h3 class="subtitle-2 my-0 font-weight-light overline gray--text">Non Member</h3> -->
             </div>
+            <v-list two-line>
+              <v-list-item @click>
+                <v-list-item-action></v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title class="subtitle-2">Carlos Joniel Santiago</v-list-item-title>
+                  <v-list-item-subtitle>#302678</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-divider inset></v-divider>
             <v-list two-line>
               <v-list-item @click>
                 <v-list-item-icon>
@@ -75,6 +86,46 @@
         <v-card class="pa-3" outlined tile>
           <h3 class="subtitle-1 grey--text text--darken-3">Forms & Letters</h3>
           <v-divider class="mb-5"></v-divider>
+          <v-card class="mx-auto" flat max-width="500">
+            <v-sheet class="pa-0 white">
+              <v-text-field
+                v-model="search"
+                label="Search Directory"
+                outlined
+                dense
+                hide-details
+                clearable
+                clear-icon="mdi-close-circle-outline"
+              ></v-text-field>
+              <v-checkbox v-model="caseSensitive"  hide-details label="Case sensitive search"></v-checkbox>
+            </v-sheet>
+            <v-card-text>
+              <v-treeview v-model="selectedDocument" 
+              :items="items" 
+              :search="search" 
+              :filter="filter" 
+              :open.sync="open" 
+              open-on-click 
+              selected-color="blue darken-1"
+              selectable>
+                <!-- <template v-slot:prepend="{ item }">
+                  <v-icon
+                    v-if="item.children"
+                    v-text="`mdi-${item.id === 1 ? 'folder' : 'folder-network'}`"
+                  ></v-icon>
+                </template> -->
+                <template v-slot:prepend="{ item, open }">
+                  <v-icon v-if="!item.file">
+                    {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+                  </v-icon>
+                  <v-icon v-else>
+                    {{ files[item.file] }}
+                  </v-icon>
+                </template>
+              </v-treeview>
+              <v-btn block class="mt-5" color="blue darken-3" dark>PRINT SELECTED DOCUMENTS</v-btn>
+            </v-card-text>
+          </v-card>
         </v-card>
       </v-col>
       <v-col>
@@ -85,8 +136,8 @@
             <v-data-table
               dense
               :headers="headers"
-              :items="desserts"
-              item-key="name"
+              :items="lastVisits"
+              item-key="id"
               class="elevation-0"
             ></v-data-table>
           </template>
@@ -99,99 +150,121 @@
 <script>
 export default {
   data: () => ({
-    desserts: [
+    // Last Visits Table
+    lastVisits: [
       {
-        name: "Frozen Yogurt",
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: "1%"
+        id: 1,
+        date: "12/25/2019",
+        hour: "1:30PM",
+        attended_by: "Mark Zuckerberg",
+        service: "General Services -> Account Cancellation"
       },
       {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: "1%"
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: "7%"
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: "8%"
-      },
-      {
-        name: "Gingerbread",
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: "16%"
-      },
-      {
-        name: "Jelly bean",
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: "0%"
-      },
-      {
-        name: "Lollipop",
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: "2%"
-      },
-      {
-        name: "Honeycomb",
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: "45%"
-      },
-      {
-        name: "Donut",
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        iron: "22%"
-      },
-      {
-        name: "KitKat",
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        iron: "6%"
+        id: 2,
+        date: "12/27/2019",
+        hour: "2:00PM",
+        attended_by: "Mark Zuckerberg",
+        service: "General Services -> Account Renovation"
       }
     ],
     headers: [
       { text: "Date", align: "left", value: "date" },
       { text: "Attended at:", value: "hour" },
       { text: "Attended by:", value: "attended_by" },
-      { text: "Service received", value: "service" },
-    ]
+      { text: "Service received", value: "service" }
+    ],
+    // Letters & Forms
+    selectedDocument: [],
+    files: {
+        html: 'mdi-language-html5',
+        js: 'mdi-nodejs',
+        json: 'mdi-json',
+        md: 'mdi-markdown',
+        pdf: 'mdi-file-pdf',
+        png: 'mdi-file-image',
+        txt: 'mdi-file-document-outline',
+        xls: 'mdi-file-excel',
+      },
+    items: [
+      {
+            id: 2,
+            name: "Cumplimiento",
+            children: [
+              {
+                id: 201,
+                name: "Document 1",
+                file: 'png',
+              },
+              {
+                id: 202,
+                name: "Document 2",
+                file: 'png',
+              },
+              {
+                id: 203,
+                name: "Document 3",
+                file: 'png',
+              },
+              {
+                id: 204,
+                name: "Document 4",
+                file: 'png',
+              },
+              {
+                id: 205,
+                name: "Document 5",
+                file: 'png',
+              }
+            ]
+          },
+          {
+            id: 3,
+            name: "Contabilidad",
+            children: [
+              {
+                id: 301,
+                name: "Document 6",
+                file: 'png',
+              },
+              {
+                id: 302,
+                name: "Document 7",
+                file: 'png',
+              }
+            ]
+          },
+          {
+            id: 4,
+            name: "Prestamos Personales",
+            children: [
+              {
+                id: 401,
+                name: "Document 8",
+                file: 'png',
+              },
+              {
+                id: 402,
+                name: "Document 9",
+                file: 'png',
+              },
+              {
+                id: 403,
+                name: "Document 10",
+                file: 'png',
+              }
+            ]
+      }
+    ],
+    open: [1, 2],
+    search: null,
+    caseSensitive: false
   }),
-  mounted() {
-    const self = this;
-
-    self.$store.commit("setBreadcrumbs", [{ label: "Dashboard", name: "" }]);
+  computed: {
+    filter() {
+      return this.caseSensitive
+        ? (item, search, textKey) => item[textKey].indexOf(search) > -1
+        : undefined;
+    }
   }
 };
 </script>
